5b1aab3f8f0fd18fccc7b80f6d6ceb7d77314577
Tiffany Carter <tafby88@gmail.com>
2019-01-29 18:20:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  # Class body
+ 
+  # Instance Method Definition
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+    puts "The dog is sitting."
+  end
+end
+ 
+fido = Dog.new
+fido.bark #> "Woof!"
